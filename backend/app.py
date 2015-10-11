5ead1de35ae00892f977927f1e27849bd9ab8924
Evgeny <evgorchakov@gmail.com>
2015-10-10 19:43:41 -0700

    Added wolfram stuff + made app much much better [Max just relax]

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 from flask import Flask, jsonify, render_template, request
+import wolframalpha
 
 app = Flask(__name__)
+
+# WOLFRAM API
+WOLFRAM_APP_ID = 'KJKLV3-YJ959ULJVY'
+
+client = wolframalpha.Client(WOLFRAM_APP_ID)
 
 ACTIVITY_MET_VALUES = {
     'running': 8,
@@ -10,10 +16,21 @@ ACTIVITY_MET_VALUES = {
     'cycling': 6,
 }
 
+def get_calories_from_wa(name):
+    query = 'calories in {}'.format(name)
+    print('WolframAlpha query: {}'.format(query))
+    res = client.query(query)
+    pod = res.pods[1]
+    calories = float(pod.text.split()[0])
+    return calories
+
 @app.route("/burn", methods=['GET'])
 def burn():
     # calories = met * weight * hours
-    calories = float(request.args['calories'])
+    calories = float(
+        request.args.get('calories') or
+        get_calories_from_wa(request.args['name'])
+    )
     weight = float(request.args['weight'])
     data = [{name: calories / (met * weight) for (name, met) in ACTIVITY_MET_VALUES.items()}]
     return jsonify({'activities': data}), 200
