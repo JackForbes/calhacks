aad28642c443be99ff9dc3f6a74d41cf79523694
Evgeny <evgorchakov@gmail.com>
2015-10-10 19:06:41 -0700

    CRITICAL UPDATE YO

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+
+from flask import Flask, jsonify, render_template, request
+
 app = Flask(__name__)
+
+ACTIVITY_MET_VALUES = {
+    'running': 150,
+    'walking': 100,
+    'cycling': 125,
+}
+
+@app.route("/burn", methods=['GET'])
+def burn():
+    # calories = met * weight * hours
+    calories = float(request.args['calories'])
+    weight = float(request.args['weight'])
+    data = [{name: float(calories) / (met * weight) for (name, met) in ACTIVITY_MET_VALUES.items()}]
+    return jsonify({'activities': data}), 200
 
 @app.route("/")
 def hello():
